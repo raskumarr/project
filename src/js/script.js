2bde6f1c57636c1333fc584947bf7351946c948f
Alexey <i@gerasimov-alex.ru>
2022-11-17 22:01:34 +0500

    validation

--- src/js/script.js
+++ src/js/script.js
@@ -48,6 +48,39 @@ $(document).ready(function(){
       $(this).on('click', function () {
         $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
         $('.overlay, #order').fadeIn('fast');
-      });
+      })
   });
+
+  function validateForms(form){
+    $(form).validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 2
+            },
+            phone: "required",
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages: {
+            name: {
+                required: "Пожалуйста, введите свое имя",
+                minlength: jQuery.validator.format("Введите {0} символа!")
+              },
+            phone: "Пожалуйста, введите свой номер телефона",
+            email: {
+              required: "Пожалуйста, введите свою почту",
+              email: "Неправильно введен адрес почты"
+            }
+        }
+    });
+};
+
+validateForms('#promo-form');
+validateForms('#promo form');
+validateForms('#order form');
+
+$('input[name=phone]').mask("+7 (999) 999-99-99");
 });
